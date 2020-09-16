09d866ec80c712d158775c20abb1c762cb85e0bc
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-09-16 16:27:46 +0100

    deploying...

--- client/server.js
+++ client/server.js
@@ -3,9 +3,9 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
