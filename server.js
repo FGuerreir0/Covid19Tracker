ad65445259229b6a35ff4f4647944fa13b87538d
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-09-16 16:29:16 +0100

    deploying...

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 app.listen(port);
