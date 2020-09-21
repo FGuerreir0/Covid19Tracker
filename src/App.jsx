fe6423754735e091154a5752a2b4079114d8fdb9
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-09-22 00:38:13 +0100

    Added Footer - Ready to share with Social Apps 🚀🚀

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.scss';
 
 //IMPORT COMPONENTS
 import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 export default function App() {
   return (
     <>
       <div className='app'>
         <Home />
+        <Footer />
       </div>
     </>
   );
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.scss';
+
+import { ExternalLink } from 'react-external-link';
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <small>
+        <span role='img' aria-label='Dev with Mask'>
+          😷
+        </span>{' '}
+        Made with all protection by{' '}
+        <ExternalLink href='https://www.linkedin.com/in/fabiofsguerreiro/'>
+          <span>Fábio Guerreiro</span>
+        </ExternalLink>{' '}
+        @ All Rights Reserved 2020{' '}
+        <span role='img' aria-label='Dev with Mask'>
+          😷
+        </span>{' '}
+      </small>
+    </div>
+  );
+}
